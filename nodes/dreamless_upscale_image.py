4757c1b13203b1ea23cd9c06f8e895dcb3f3fc74
Tiago Pereira <tiagonet50@gmail.com>
2026-06-24 21:07:01 -0300

    dreamless_upscale_image.py

--- nodes/dreamless_upscale_image.py
+++ nodes/dreamless_upscale_image.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 MSG_PREFIX = "\33[1m\33[34m[Dreamless] \33[0m"
 
 
-class Dreamless_Upscale_Image:    
+class Dreamless_Upscale_Image:
     @classmethod
     def INPUT_TYPES(cls):
         return {
