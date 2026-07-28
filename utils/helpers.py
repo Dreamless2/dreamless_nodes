33a49a73fd018608242eaf8bfb96c24c6916b54e
Dreamless2 <tiagonet50@gmail.com>
2026-07-27 22:25:10 -0300

    helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -117,7 +117,7 @@ def is_hf(value: str) -> bool:
 
 def parse_hf(value: str):
     """
-    Extract (repo_id, filename, revision) from URL HF.
+    Parses repo_id, filename, revision) from URL HF.
 
     Accept the following formats:
         hf:black-forest-labs/FLUX.1-dev/flux1-dev.safetensors
